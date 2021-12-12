d09c026d63e3622323c9dfc0fbffa507b464d0b4
ryanczj05 <ryanczj05@gmail.com>
2021-12-12 08:26:10 +0000

    commit 5

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-xl navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">My Mood Tracker</a>
+      <a class="navbar-brand" href="/">My Mood Tracker</a>
      
     </div>
   </nav>
